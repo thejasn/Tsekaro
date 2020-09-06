27638298e4f12599ecf3a97c6fbca7d04bd1200e
Thejas Nanjunda <thejasn.416@gmail.com>
2020-09-06 14:36:27 +0530

    vue setup done and split component files

---
+++ frontend/src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template src='./HelloWorld.html'></template>
+<style scoped src="./HelloWorld.css"></style>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
